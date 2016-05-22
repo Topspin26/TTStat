41986c743929b39fcc5472caf23e6181086a5d7c
Topspin26 <zhelubenkovalexandr@gmail.com>
2016-05-23 00:22:20 +0300

    Исправлен перебор файлов директории

--- bkfon_deleteDuplicates.py
+++ bkfon_deleteDuplicates.py
@@ -1,72 +1,73 @@
 import time
-from os import walk
+import os
 from lxml import html
 from lxml import etree
 
 def main():
     dir = 'D:/Programming/SportPrognoseSystem/BetsWinner/data/bkfon/live'
 
-    for f in walk(dir):
-        for ff in f[2]:
-            if ff[:7] == 'segment':
-                trSegmentLine = None
-                trEventLines = None
-                trEventS = None
-                lastEventId = None
-                eventsDict = dict()
-                with open(dir + '/' + ff, 'r', encoding='utf-8') as filename, \
-                     open(dir + '/clean/' + ff, 'w', encoding='utf-8') as fout:
-                    k = 0
-                    for line in filename:
-                        tokens = line.split('\t')
-                        time = ''
-                        if len(tokens) == 2:
-                            time = tokens[0]
-                            s = tokens[1]
+    for f in os.listdir(dir):
+#        print(os.path.isfile(os.path.join(dir, f)))
+        if f[:7] == 'segment':
+            trSegmentLine = None
+            trEventLines = None
+            trEventS = None
+            lastEventId = None
+            eventsDict = dict()
+            print(f)
+            with open(dir + '/' + f, 'r', encoding='utf-8') as filename, \
+                 open(dir + '/clean/' + f, 'w', encoding='utf-8') as fout:
+                k = 0
+                for line in filename:
+                    tokens = line.split('\t')
+                    time = ''
+                    if len(tokens) == 2:
+                        time = tokens[0]
+                        s = tokens[1]
+                    else:
+                        s = line
+                    tr = html.fromstring(s)
+                    cl = tr.get('class')
+                    
+                    if cl.find('trEventChild') == -1:
+                        if not (lastEventId is None):
+                            printFl = 1
+                            if lastEventId in eventsDict:
+                                if eventsDict[lastEventId] == trEventS:
+                                    printFl = 0
+                            if printFl == 1:
+                                if trSegmentLine != '':
+                                    fout.write(trSegmentLine)
+                                    trSegmentLine = ''
+                                fout.write(trEventLines)
+                            eventsDict[lastEventId] = trEventS
+                        if cl != 'trSegment':
+                            lastEventId = tr.get('id')
                         else:
-                            s = line
-                        tr = html.fromstring(s)
-                        cl = tr.get('class')
-                        
-                        if cl.find('trEventChild') == -1:
-                            if not (lastEventId is None):
-                                printFl = 1
-                                if lastEventId in eventsDict:
-                                    if eventsDict[lastEventId] == trEventS:
-                                        printFl = 0
-                                if printFl == 1:
-                                    if trSegmentLine != '':
-                                        fout.write(trSegmentLine)
-                                        trSegmentLine = ''
-                                    fout.write(trEventLines)
-                                eventsDict[lastEventId] = trEventS
-                            if cl != 'trSegment':
-                                lastEventId = tr.get('id')
-                            else:
-                                lastEventId = None
-                            trEventLines = ''
-                            trEventS = ''
-                        
-                        if cl == 'trSegment':
-                            trSegmentLine = line
-                        else:
-                            trEventLines += line
-                            trEventS += s
-                        
-                        k += 1
-                        if k % 1000 == 0:
-                            print(k)
+                            lastEventId = None
+                        trEventLines = ''
+                        trEventS = ''
+                    
+                    if cl == 'trSegment':
+                        trSegmentLine = line
+                    else:
+                        trEventLines += line
+                        trEventS += s
+                    
+                    k += 1
+                    if k % 1000 == 0:
+                        print(k)
 #                        if k == 2000:
 #                            break
-                    if not (lastEventId is None):
-                        printFl = 1
-                        if lastEventId in eventsDict:
-                            if eventsDict[lastEventId] == trEventS:
-                                printFl = 0
-                        if printFl == 1:
-                            if trSegmentLine != '':
-                                fout.write(trSegmentLine)
-                            fout.write(trEventLines)
+                if not (lastEventId is None):
+                    printFl = 1
+                    if lastEventId in eventsDict:
+                        if eventsDict[lastEventId] == trEventS:
+                            printFl = 0
+                    if printFl == 1:
+                        if trSegmentLine != '':
+                            fout.write(trSegmentLine)
+                        fout.write(trEventLines)
 
 if __name__ == "__main__":
     main()
