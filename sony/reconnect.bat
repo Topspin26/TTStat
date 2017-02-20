a1f80801e28bd791e07db7e72ec581b5bf078570
Topspin26 <zhelubenkovalexandr@gmail.com>
2017-02-20 14:27:44 +0300

    sony scrapper scripts

---
+++ sony/reconnect.bat
@@ -0,0 +1,2 @@
+netsh interface set interface name = "Беспроводное сетевое соединение" admin = DISABLED
+netsh interface set interface name = "Беспроводное сетевое соединение" admin = ENABLED
---
+++ sony/run_scrapper.bat
@@ -0,0 +1,2 @@
+set PATH=%PATH%;C:/Python33;C:/Python33/Scripts;C:/Python33/Lib;C:/Python33/libs
+python scrapper.py
