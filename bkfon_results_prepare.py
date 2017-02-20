a7cb10d3a668c37a6f7ea28f196b0eeb8b4d979e
Topspin26 <zhelubenkovalexandr@gmail.com>
2017-02-20 13:04:53 +0300

    add bkfon_results source

---
+++ bkfon_results_prepare.py
@@ -0,0 +1,195 @@
+from os import walk
+from lxml import html
+from common import *
+from Entity import *
+import time
+import datetime as datetime
+import random
+import re
+
+def getMatches(corrections, wrongLines):
+#    pattern = re.compile('|'.join(corrections.keys()))
+    matches = []
+    for f in walk('data/bkfon/results'):
+        for ff in f[2]:
+            print(ff)
+            if ff.find('old') != -1:
+                continue
+#            if ff.find('2016-12') == -1 and ff.find('2017-') == -1:
+#                continue
+            with open('data/bkfon/results/' + ff, 'r', encoding='utf-8') as fin:
+                for line in fin:
+                    table = html.fromstring(line)
+                    trs = table.xpath("*//tr")
+                    flTT = 0
+                    compName = ''
+                    for tr in trs:
+                        if tr.get('class') == 'sectCaption':
+                            s = tr.xpath('.//th/text()')[0].replace(u'\xa0', ' ')
+                            compName = s
+                            if s.find('Наст. теннис') != -1 and s.find('TT-CUP') == -1:
+                                flTT = 1
+                                #print(s)
+                            else:
+                                flTT = 0
+                        else:
+                            if flTT == 1:
+                                arr = [re.sub(' +', ' ', e.replace(u'\xa0', ' ')) for e in tr.xpath('.//text()')]
+                                s0 = '\t'.join(arr)
+                                for k,v in corrections:
+                                    if k.find(';') != -1:
+                                        if k.split(';')[0] == ff[:10]:
+                                            arr = [e.replace(k.split(';')[1], v) for e in arr]
+                                    else:
+                                        arr = [e.replace(k, v) for e in arr]
+                                if '\t'.join(arr) != s0:
+                                    print(s0 + '\n' + '\t'.join(arr) + '\n')
+#                                if s0.find('(ж)') != -1:
+#                                    print(s0)
+                                #arr = [pattern.sub(lambda x: corrections[x.group()], e) for e in arr]
+
+                                names = arr[2].strip().split(' - ')
+                                if len(names) != 2:
+                                    print(arr)
+                                    continue
+                                if names[0].lower().find('game') != -1:
+                                    continue
+                                timeArr = arr[1].split(' ')
+                                if len(timeArr) == 2:
+                                    time = timeArr[1].strip()
+                                else:
+                                    time = time.strip()
+                                if arr[4] != 'отмена':
+                                    matches.append(Match(ff[:10],
+                                                         [[e.strip() for e in names[0].strip().split('/')],
+                                                          [e.strip() for e in names[1].strip().split('/')]],
+                                                         setsScore=arr[3],
+                                                         pointsScore=arr[4].replace('(', '').\
+                                                            replace(')', '').replace(' ', ';').replace('-', ':'),
+                                                         time=time,
+                                                         compName=compName))
+    return matches
+
+def getMatchesPlayers(matches):
+    res = dict()
+    for match in matches:
+        for i in range(2):
+            for player in match.players[i]:
+                updateDict(res, player)
+    return res
+
+def updateDict(d, k):
+    if k in d:
+        d[k] += 1
+    else:
+        d[k] = 1
+
+def main():
+    filenameGlobalPlayersMen = r'D:\Programming\SportPrognoseSystem\BetsWinner\prepared_data\players_men.txt'
+    (mIdG, mId2G) = readPlayersInv(filenameGlobalPlayersMen)
+    filenameGlobalPlayersWomen = r'D:\Programming\SportPrognoseSystem\BetsWinner\prepared_data\players_women.txt'
+    (wIdG, wId2G) = readPlayersInv(filenameGlobalPlayersWomen)
+
+    corrections = readCorrectionsList(r'D:\Programming\SportPrognoseSystem\BetsWinner\data\bkfon\corrections.txt')
+    wrongLines = []
+    matches = getMatches(corrections, wrongLines)
+    print(len(matches))
+    players = getMatchesPlayers(matches)
+    empty = dict()
+    m = dict()
+    w = dict()
+    mw = dict()
+    for e in players:
+        if e in mId2G and e in wId2G:
+            updateDict(mw, e)
+            print('MW ' + e)
+        if e in mId2G and not (e in wId2G):
+            print('M ' + e)
+            updateDict(m, e)
+        if not (e in mId2G) and (e in wId2G):
+            print('W ' + e)
+            updateDict(w, e)
+        if not (e in mId2G) and not (e in wId2G):
+            print('? ' + e)
+            updateDict(empty, e)
+    playersMW = dict()
+    playersMatches = dict()
+    for player in players:
+        playersMW[player] = [0, 0, 0]
+    for match in matches:
+        if len(match.players[0]) == 1:
+            pl1 = match.players[0][0]
+            pl2 = match.players[1][0]
+            if pl1 in m:
+                playersMW[pl2][0] += 1
+            elif pl1 in w:
+                playersMW[pl2][1] += 1
+            if pl2 in m:
+                playersMW[pl1][0] += 1
+            elif pl2 in w:
+                playersMW[pl1][1] += 1
+            playersMW[pl1][2] += 1
+            playersMW[pl2][2] += 1
+            if not (pl1 in playersMatches):
+                playersMatches[pl1] = []
+            playersMatches[pl1].append(match.toStr())
+            if not (pl2 in playersMatches):
+                playersMatches[pl2] = []
+            playersMatches[pl2].append(match.toStr())
+
+    with open(r'D:\Programming\SportPrognoseSystem\BetsWinner\prepared_data\bkfon\bkfon_players_x_mw.txt', 'w', encoding = 'utf-8') as fout:
+        for e in sorted(playersMW.items(), key = lambda x: -x[1][2]):
+#        for e in sorted(playersMW.items(), key = lambda x: (x[1][0] + 1) / (x[1][2] + 2)):
+            if (e[0] in empty) and e[1][2] > 0:
+                print(e)
+                fout.write(e[0] + '\t' + str(e[1]) + '\t' + '\t'.join(playersMatches[e[0]]) + '\n')
+
+    prefix = r'D:\Programming\SportPrognoseSystem\BetsWinner\prepared_data\bkfon\\'
+    with open(prefix + 'bkfon_players_men.txt', 'w', encoding = 'utf-8') as fout:
+        for e in sorted(m.keys()):
+            fout.write(e + '\t' + ';'.join(mId2G[e]) + '\n')
+    with open(prefix + 'bkfon_players_women.txt', 'w', encoding = 'utf-8') as fout:
+        for e in sorted(w.keys()):
+            fout.write(e + '\t' + ';'.join(wId2G[e]) + '\n')
+    with open(prefix + 'bkfon_players_mw.txt', 'w', encoding = 'utf-8') as fout:
+        for e in sorted(mw.keys()):
+            fout.write(e + '\t' + ';'.join(mId2G[e]) + '\t' + ';'.join(wId2G[e]) + '\n')
+    with open(prefix + 'bkfon_players_x.txt', 'w', encoding = 'utf-8') as fout:
+        for e in sorted(empty.keys()):
+            fout.write(e + '\t' + '?' + '\n')
+
+    with open(prefix + 'all_results.txt', 'w', encoding='utf-8') as fout:
+        fout.write('\t'.join(['date', 'time', 'compName', 'id1', 'id2', 'setsScore', 'pointsScore', 'name1', 'name2']) + '\n')
+        for match in matches:
+            if match.flError == 0:
+#                print(match.toStr())
+                flError = 0
+                ids = [[], []]
+                for i in range(2):
+                    for player in match.players[i]:
+                        if player in mw:
+                            flError = 1
+                        elif (player in m):
+                            if len(mId2G[player]) == 1:
+                                ids[i].append(mId2G[player][0])
+                            else:
+                                if player.find('Желуб') != -1:
+                                    print(match.toStr())
+                                flError = 1
+                        elif (player in w):
+                            if len(wId2G[player]) == 1:
+                                ids[i].append(wId2G[player][0])
+                            else:
+                                flError = 1
+                        else:
+                            flError = 1
+                if flError == 0:
+                    resTokens = match.toArr()
+                    resTokens.append(resTokens[3])
+                    resTokens.append(resTokens[4])
+                    resTokens[3] = ';'.join(ids[0])
+                    resTokens[4] = ';'.join(ids[1])
+                    fout.write('\t'.join(resTokens) + '\n')
+
+if __name__ == "__main__":
+    main()
---
+++ bkfon_results_scrapper.py
@@ -0,0 +1,90 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from datetime import datetime
+import random
+import os
+import re
+from subprocess import Popen
+
+
+def initDriver(url, driver):
+    driver = webdriver.Chrome('chromedriver_win32/chromedriver')#, port = 5938)
+    driver.get(url)
+    time.sleep(1 + random.random()) # Let the user actually see something!
+    return driver
+
+def getResults(curDate):
+    s = None
+    try:
+        with open('data/bkfon/results/' + curDate + '.txt', 'r', encoding = 'utf-8') as fin:
+            s = next(fin)
+    except:
+        s = None
+    return s
+
+def main():
+    url = 'https://www.bkfon.ru/results'
+    driver = None
+    driver = initDriver(url, driver)
+
+    monthname2Num = {'янв':1,'фев':2,'мар':3,'апр':4,'май':5,'июн':6,'июл':7,'авг':8,'сен':9,'окт':10,'ноя':11,'дек':12}
+
+    tDate = datetime.now().strftime("%Y-%m-%d")
+
+    flLast = 1
+    flExit = 0
+    while (flExit == 0):
+        month = driver.find_element_by_class_name('ui-datepicker-month').get_attribute('innerHTML')
+        month = monthname2Num[month.lower()[:3]]
+        print(month)
+        year = driver.find_element_by_class_name('ui-datepicker-year').get_attribute('innerHTML')
+        print(year)
+        active_days = driver.find_elements(By.XPATH, "//*[@data-handler='selectDay']")
+        nd = len(active_days)
+        print(len(active_days))
+        if year == '2017':
+            for i in range(nd - flLast):
+                if month != 2:
+                    continue
+                curDate = year + '-' + str(month).zfill(2) + '-' + str(i + 1).zfill(2)
+                print(curDate)
+                filename ='data/bkfon/results/' + curDate + '.txt'
+                sLast = getResults(curDate)
+
+                print(active_days[i].get_attribute('innerHTML'))
+                active_days[i].click()
+                time.sleep(5 + random.random())
+                active_days = driver.find_elements(By.XPATH, "//*[@data-handler='selectDay']")
+                s = driver.find_element_by_id('resultDiv').get_attribute('innerHTML')
+                if sLast is None:
+                    with open(filename, 'w', encoding = 'utf-8') as fout:
+                        fout.write(s)
+                elif sLast.strip() != s.strip():
+                    arr1 = s.split('class="resultTd"')
+                    arr2 = sLast.split('class="resultTd"')
+                    flNewInfo = 0
+                    for e in arr2:
+                        if not (e in arr1):
+                            flNewInfo = 1
+                            print(e)
+                    if flNewInfo == 1:
+                        print(curDate + ' NEW INFO')
+                        with open(filename, 'w', encoding='utf-8') as fout:
+                            fout.write(s)
+                        with open(filename[:-4] + '_old' + tDate + '.txt', 'w', encoding='utf-8') as fout:
+                            fout.write(sLast)
+        #                    return
+        else:
+            flExit = 1
+            break
+        driver.find_element_by_class_name('ui-datepicker-prev').click()
+        time.sleep(5 + random.random())
+        flLast = 0
+        continue
+    return
+
+
+if __name__ == "__main__":
+    main()
