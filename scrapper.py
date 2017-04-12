0deba48bae15488e2520fe164abc902f9fe2134e
Topspin26 <zhelubenkovalexandr@gmail.com>
2017-04-13 00:12:08 +0300

    new live scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,96 +1,110 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
 import time
 from datetime import datetime
 import random
 import re
+import os
+from subprocess import Popen
 
+chrome_options = Options()
+#chrome_options.add_extension('chromedriver_win32/extension_2_0_327.crx')
+#chrome_options.add_extension('chromedriver_win32/extension_3_0_0.crx')
 
-def initDriver(url):
-    driver = webdriver.Chrome('chromedriver_win32/chromedriver', port = 5938)
-    driver.get(url)
-    time.sleep(3 + random.random()) # Let the user actually see something!
+def initDriver(url, driver):
+    #p = Popen("reconnect.bat")
+    #p.wait()
+    #time.sleep(15)
+    if (True):#driver is None:
+        try:
+            driver.quit()
+        except:
+            pass
+        driver = webdriver.Chrome('chromedriver_win32/chromedriver', chrome_options=chrome_options)
+#        driver.get('chrome-extension://gjknjjomckknofjidppipffbpoekiipm/popup.html')
+#        element = driver.find_element_by_id('connect')
+#        element.click()
+        driver.get(url)
+    else:
+        try:
+            driver.refresh()
+        except:
+            driver = webdriver.Chrome('chromedriver_win32/chromedriver', chrome_options=chrome_options)
+            driver.get(url)
+            
+    time.sleep(2 + random.random()) # Let the user actually see something!
+    #iframes = driver.find_elements_by_xpath("//iframe")
+    #driver.switch_to_frame(iframes[1])
     return driver
 
 def main():
 
+
+    url = 'https://www.fonbet.ru/#/live' #31.03.2016
+    url1 = 'https://www.fonbet.ru/#/bets'
+
 #    url = 'https://live.fonbet.com/?locale=ru'
 #    url = 'https://live.bkfon-bet.com/?locale=ru'
-    url = 'http://www.bkfon.ru/ru/live/' #31.03.2016
-    driver = initDriver(url)
+#    url = 'http://www.bkfon.ru/ru/live/' #31.03.2016
+#    url = 'https://live.bet-in-fon.com/?locale=ru' #25.06.2016
+    driver = None
+    driver = initDriver(url, driver)
+    #driver = initDriver(url, driver)
 #    e = driver.find_element_by_class_name('trSegment')
-    iframes = driver.find_elements_by_xpath("//iframe")
 #    print(iframes)            
-    driver.switch_to_frame(iframes[1])
 #    output = driver.page_source
 #    print(output)
-        
-
+    
     k = 0
     kError = -1
     cntError = 0
+    lastEventsData = []
+    eventsData = []
+    eqCnt = 0
+    tg = 60 * 60
     while (1):
         k += 1
-        print(k)
-        if k % 1440 == 0:
-            driver.quit()
-            driver = initDriver(url)
-            iframes = driver.find_elements_by_xpath("//iframe")
-            driver.switch_to_frame(iframes[1])
-            
-        timeout = 60
-        eventsData = []
-        curTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print((k, eqCnt))
         try:
-            lineTable = driver.find_element_by_id('lineTable')
+            if eqCnt == 16:
+                #driver.quit()
+                driver = initDriver(url, driver)
+                eqCnt = 0
+                
+            timeout = 60
+            eventsData = []
+            curTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            lineTable = driver.find_element_by_xpath('//section[@class="table__inner"]/table[@class="table"]')
             print('lineTable - ok')
             sout = lineTable.get_attribute('outerHTML')
             print(len(sout))
-            indexesSegment = [[m.start(), m.end(), 0] for m in re.finditer('<tr [^>]* id=\"segment(.)*?</tr>', sout)]
-            indexesEvent = [[m.start(), m.end(), 1] for m in re.finditer('<tr [^>]* id=\"event(.)*?</tr>', sout)]
+            indexesSegment = [[m.start(), m.end(), 0] for m in re.finditer('<tr class=\"table__row _type_segment(.)*?</tr>', sout)]
+            indexesEvent = [[m.start(), m.end(), 1] for m in re.finditer('<tr class=\"table__row"(.)*?</tr>', sout)]
             indexes = sorted(indexesSegment + indexesEvent, key=lambda x: x[0])
-            print(indexes)
+            #print(indexes)
 
             flLine = 0
             segmentId = ''
             for ind in indexes:
                 if ind[2] == 0:
                     flLine = 0
-                    if sout[ind[0]:ind[1]].find('sport3088') != -1:
+#                    print(sout[ind[0]:ind[1]])
+                    if sout[ind[0]:ind[1]].find('sport_3088') != -1:
                         flLine = 1
-                        ss = re.findall('segment[0-9]*', sout[ind[0]:ind[1]])
-                        segmentId = ss[0]
-#                        print(segmentId)
+#                        ss = re.findall('segment[0-9]*', sout[ind[0]:ind[1]])
+                        segmentId = sout[ind[0]:ind[1]].split('<span class="table__title-text">')[1].split('<')[0]
+                        print(segmentId)
                 if flLine == 1:
                     if segmentId != '':
                         eventsData.append([segmentId, sout[ind[0]:ind[1]]])
                         timeout = 5
 #            for e in eventsData:
 #                print(e)
-            print('-----------')
-            '''
-            trs = lineTable.find_elements_by_xpath('//tr')
-            flLine = 0
-            segmentId = ''
-            for tr in trs:
-                try:
-                    s = tr.get_attribute('outerHTML')
-                    if s.find('trSegment') != -1:
-                        flLine = 0
-                        if s.find('sport3088') != -1:
-                            flLine = 1
-                            segmentId = tr.get_attribute('id')
-                    if flLine == 1:
-#                        print(s)
-                        if segmentId != '':
-                            eventsData.append([segmentId, s])
-                            timeout = 5
-                except Exception as e:
-                    flLine = 0
-#                    print('error1')
-#                    print(str(e))
-#                    fout.write('error1\n')
-            '''
+            print(len(eventsData))
+                
         except Exception as e:
             eventsData.append(['error', 'error2\n' + str(e)])
             print('error2')
@@ -99,27 +113,111 @@ def main():
                 cntError += 1
             else:
                 cntError = 1
+                timeout = 5
             if cntError == 5:
-                timeout = 600
+                timeout = 60
                 cntError = 0
             kError = k
-            driver.quit()
-            driver = initDriver(url)
+            try:
+                #driver.quit()
+                driver = initDriver(url, driver)
+            except Exception as e:
+                foo = 0
+
+        try:
+            lastEventId = ''
+            fout = ''
+            for e in eventsData:
+                if e[0] != lastEventId:
+                    lastEventId = e[0]
+                    if fout != '':
+                        fout.close()
+                    if e[0] != 'error':
+                        e[0] = e[0]
+                    if not os.path.exists('data/' + curTime[:10]):
+                        os.mkdir('data/' + curTime[:10])
+                    fout = open('data/' + curTime[:10] + '/' + e[0] + '.txt', 'a', encoding='utf-8')
+                fout.write(curTime + '\t' + e[1] + '\n')
+            if fout != '':
+                fout.close()
+        except:
+            try:
+                fout.close()
+            except:
+                foo = 0
+            print('writing error')
+
         
-        '''
-        lastEventId = ''
-        fout = ''
-        for e in eventsData:
-            if e[0] != lastEventId:
-                lastEventId = e[0]
+        if ('\t'.join([e[1] for e in eventsData]) == '\t'.join([e[1] for e in lastEventsData])):
+            eqCnt += 1
+        else:
+            eqCnt = 0
+        lastEventsData = eventsData.copy()
+        time.sleep(timeout)
+        tg += timeout
+        if tg >= 60 * 60:
+            try:
+                driver.get(url1)
+                time.sleep(5 + random.random())
+            
+                curTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                eventsData = []
+                
+                lineTable = driver.find_element_by_xpath('//section[@class="table__inner"]/table[@class="table"]')
+                print('lineTable - ok')
+                sout = lineTable.get_attribute('outerHTML')
+                print(len(sout))
+                indexesSegment = [[m.start(), m.end(), 0] for m in re.finditer('<tr class=\"table__row _type_segment(.)*?</tr>', sout)]
+                indexesEvent = [[m.start(), m.end(), 1] for m in re.finditer('<tr class=\"table__row"(.)*?</tr>', sout)]
+                indexes = sorted(indexesSegment + indexesEvent, key=lambda x: x[0])
+            
+                flLine = 0
+                segmentId = ''
+                for ind in indexes:
+                    if ind[2] == 0:
+                        flLine = 0
+                        if sout[ind[0]:ind[1]].find('sport_3088') != -1:
+                            flLine = 1
+                            segmentId = sout[ind[0]:ind[1]].split('<span class="table__title-text">')[1].split('<')[0]
+                            print(segmentId)
+                    if flLine == 1:
+                        if segmentId != '':
+                            eventsData.append([segmentId, sout[ind[0]:ind[1]]])
+                print(len(eventsData))
+            except Exception as e:
+                eventsData.append(['error', 'error2\n' + str(e)])
+                print('error2')
+                print(str(e))
+
+
+            try:
+                lastEventId = ''
+                fout = ''
+                for e in eventsData:
+                    if e[0] != lastEventId:
+                        lastEventId = e[0]
+                        if fout != '':
+                            fout.close()
+                        if e[0] != 'error':
+                            e[0] = 'data_bets/' + e[0]
+                        fout = open(e[0] + '.txt', 'a', encoding='utf-8')
+                    fout.write(curTime + '\t' + e[1] + '\n')
                 if fout != '':
                     fout.close()
-                fout = open(e[0] + '.txt', 'a', encoding='utf-8')
-            fout.write(curTime + '\t' + e[1] + '\n')
-        if fout != '':
-            fout.close()
-        '''
-        time.sleep(timeout)
+            except Exception as e:
+                print(str(e))
+                try:
+                    fout.close()
+                except:
+                    foo = 0
+                print('writing error')
+
+            try:
+                driver.get(url)
+                time.sleep(5 + random.random())
+            except:
+                pass
+            tg = 0
             
     return
 
