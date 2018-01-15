3fe26dd8e8031939a0b91f29f8ac1b34ea434791
Topspin26 <zhelubenkovalexandr@gmail.com>
2018-01-16 01:29:47 +0300

    rankingCalc fix

--- rankingCalc.py
+++ rankingCalc.py
@@ -10,18 +10,13 @@ def calcWinWeight(match):
     return 0.95 * match.wins[0] + 0.05 * match.wins[1]
 
 
-def run(playersDict, matchesStorage, mw, params):
+def run(playersDict, matchesStorage, mw, params, rightDate):
     matches = matchesStorage.matches
 
     events = []
     k = 0
     for match in sorted(matches, key=lambda x: x.date):
-        fl_mw = ''
-        for e in match.ids[0] + match.ids[1]:
-            fl_mw += e[0]
-        fl_mw = ''.join(sorted(set(list(fl_mw))))
-
-        if match.isPair == 0 and fl_mw == mw:
+        if match.isPair == 0 and mw+mw == match.getMW():
             if match.sets is not None:
                 events.append([match.date, 1, k, match])
                 removeDate = (datetime.datetime.strptime(match.date, "%Y-%m-%d").date() +
@@ -37,10 +32,11 @@ def run(playersDict, matchesStorage, mw, params):
     curDate = None
     for dt, fl, k, match in sorted(events, key=lambda x: x[0] + '_' + str(x[1])):
         if curDate != dt:
-            if curDate is not None and '2016-01-01' <= curDate <= (datetime.datetime.now() + datetime.timedelta(days=1)).strftime('%Y-%m-%d'):
-                curDate = dt
-                print(curDate)
-                r = rm.calcRankings()
+            if curDate is not None and '2017-12-25' <= curDate <= rightDate:
+            # if curDate is not None and '2015-12-31' <= curDate <= rightDate:
+                outDate = (datetime.datetime.strptime(curDate, '%Y-%m-%d') + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+                print(outDate)
+                r, matchesCnt = rm.calcRankings(matchesCntBorder=params['matchesCntBorder'])
 
                 res = []
                 for e in sorted(playersDict.id2names):
@@ -48,10 +44,10 @@ def run(playersDict, matchesStorage, mw, params):
                         continue
                     id = int(e[1:]) - 1
                     if id < len(r) and r[id] == r[id]:
-                        res.append([e, playersDict.getName(e), r[id]])
+                        res.append([e, playersDict.getName(e), r[id], matchesCnt[id]])
 
                 for i, e in enumerate(sorted(res, key=lambda x: x[2], reverse=True)):
-                    rankings[params['name']].append([curDate, e[0], str(e[2]), str(i + 1)])
+                    rankings[params['name']].append([outDate, e[0], str(e[2]), str(i + 1), str(e[3])])
 
             curDate = dt
         if fl == 1:
@@ -64,7 +60,7 @@ def run(playersDict, matchesStorage, mw, params):
             break
 
     for k, v in rankings.items():
-        with open('prepared_data/rankings/rankings_' + mw + '_' + k + '.txt', 'w', encoding='utf-8') as fout:
+        with open('prepared_data/rankings/rankings_' + mw + '_' + k + '.txt', 'a', encoding='utf-8') as fout:
             for e in v:
                 fout.write('\t'.join(e) + '\n')
 
@@ -89,13 +85,17 @@ def main():
     sourcesSet1 = {'master_tour', 'liga_pro', 'challenger_series', 'local', 'ittf'}
 
     #mw = 'm'
-    params = dict()
-    params['ws'] = 365
-    params['wf'] = calcSetWeight
-    params['sources'] = sourcesSet0
-    params['name'] = 'sets_0'
-    for mw in ['m', 'w']:
-        run(playersDict, matchesStorage, mw, params)
+    rightDate = (datetime.datetime.now()).strftime('%Y-%m-%d')
+    for ws in [730]:
+        for matchesCntBorder in [1, 4]:
+            params = dict()
+            params['ws'] = ws
+            params['matchesCntBorder'] = matchesCntBorder
+            params['wf'] = calcSetWeight
+            params['sources'] = sourcesSet0
+            params['name'] = 'sets_sources=0_ws=' + str(ws) + '_matchesCntBorder=' + str(matchesCntBorder)
+            for mw in ['m', 'w']:
+                run(playersDict, matchesStorage, mw, params, rightDate)
 
 
 if __name__ == "__main__":
